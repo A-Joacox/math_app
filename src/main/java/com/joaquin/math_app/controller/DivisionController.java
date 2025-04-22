4926168a186df082725af5c502bf51ac9aff075c
joaquin_ <joacmercado@gmail.com>
2025-04-22 16:28:30 -0500

    added division

--- src/main/java/com/joaquin/math_app/controller/DivisionController.java
+++ src/main/java/com/joaquin/math_app/controller/DivisionController.java
@@ -1,4 +1,29 @@
 package com.joaquin.math_app.controller;
 
+import com.joaquin.math_app.entity.DivisionEntity;
+import com.joaquin.math_app.service.DivisionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DivisionController {
+
+    @Autowired
+    private DivisionService divisionService;  // Inject the service
+
+    @GetMapping("/division/{num1}/{num2}")
+    public ResponseEntity<Object> getDivision(@PathVariable long num1, @PathVariable long num2) {
+        try {
+            DivisionEntity divisionEntity = divisionService.calculateDivision(num1, num2);
+            return ResponseEntity.ok(divisionEntity.getResult());
+        } catch (ArithmeticException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: No se puede dividir por cero.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error interno del servidor: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/com/joaquin/math_app/service/DivisionService.java
+++ src/main/java/com/joaquin/math_app/service/DivisionService.java
@@ -1,4 +1,11 @@
 package com.joaquin.math_app.service;
 
+import com.joaquin.math_app.entity.DivisionEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DivisionService {
+    public static DivisionEntity calculateDivision(long num1, long num2) {
+        return new DivisionEntity(num1, num2); // Creates a new SubstractEntity with the sum
+    }
 }
